7378a2dd77644a040e13c5500d29ab1ad3744a15
Yoganand Chandrasekhar <chandrasekhar.y@husky.neu.edu>
2016-04-11 13:16:57 -0700

    Restructure list testing

---
+++ assignment9/src/test/java/edu/neu/ccs/cs5004/seattle/assignment9/html/HorizontalLineTest.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.neu.ccs.cs5004.seattle.assignment9.html;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author yoganandc
+ */
+public class HorizontalLineTest {
+
+    public HorizontalLineTest() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testToPrettyString() {
+    }
+
+}
