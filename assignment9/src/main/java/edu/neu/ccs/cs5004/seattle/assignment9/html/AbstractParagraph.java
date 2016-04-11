8a435fb415cb2e2c48e0a76f89bb8db2d9c37df5
Yoganand Chandrasekhar <chandrasekhar.y@husky.neu.edu>
2016-04-11 11:45:25 -0700

    Re-add hyperLinkify()

--- assignment9/src/main/java/edu/neu/ccs/cs5004/seattle/assignment9/html/AbstractParagraph.java
+++ assignment9/src/main/java/edu/neu/ccs/cs5004/seattle/assignment9/html/AbstractParagraph.java
@@ -2,14 +2,29 @@ package edu.neu.ccs.cs5004.seattle.assignment9.html;
 
 public abstract class AbstractParagraph extends AbstractElement {
 
-  public AbstractParagraph() {
-    // TODO Auto-generated constructor stub
-  }
+    public static String hyperLinkify(String value) {
+        int textBegin = value.indexOf(TEXT_BEGIN);
+        int textEnd = value.indexOf(TEXT_END);
+        int linkBegin = value.indexOf(LINK_BEGIN);
+        int linkEnd = value.indexOf(LINK_END);
 
-  @Override
-  public String toPrettyString() {
-    // TODO Auto-generated method stub
-    return null;
-  }
+        if (textBegin != -1 && textBegin < textEnd && textEnd < linkBegin && linkBegin < linkEnd) {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append(value.substring(0, textBegin));
+            stringBuilder.append("<a href=\"");
+            stringBuilder.append(value.substring(linkBegin + 1, linkEnd));
+            stringBuilder.append("\">");
+            stringBuilder.append(value.substring(textBegin + 1, textEnd));
+            stringBuilder.append("</a>");
+            stringBuilder.append(hyperLinkify(value.substring(linkEnd + 1, value.length())));
+            return stringBuilder.toString();
+        } else {
+            return value;
+        }
+    }
+    private static final String LINK_END = ")";
+    private static final String LINK_BEGIN = "(";
+    private static final String TEXT_END = "]";
+    private static final String TEXT_BEGIN = "[";
 
 }
