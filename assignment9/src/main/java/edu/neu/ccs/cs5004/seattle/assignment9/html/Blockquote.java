36b9995c39e62931099ae684d05f5a258420f767
alesyavt <alesyatrubchik@Alesyas-MacBook-Pro.local>
2016-04-11 00:54:22 -0700

    blockquotes

--- assignment9/src/main/java/edu/neu/ccs/cs5004/seattle/assignment9/html/Blockquote.java
+++ assignment9/src/main/java/edu/neu/ccs/cs5004/seattle/assignment9/html/Blockquote.java
@@ -1,5 +1,6 @@
 package edu.neu.ccs.cs5004.seattle.assignment9.html;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Blockquote extends AbstractParagraph {
@@ -10,38 +11,56 @@ public class Blockquote extends AbstractParagraph {
 
 
   public Blockquote(List<String> block) {
-    this.specialCharCount = 1;
-
+    this(block, 1);
   }
 
 
   public Blockquote(List<String> block, int specialCount) {
+    this.list = new ArrayList<>(); /// toPS
     this.specialCharCount = specialCount;
     if (specialCount(block.get(0)) == this.specialCharCount) {
-      int paragIndex = 0;
-      StringBuilder parag = new StringBuilder();
-      parag.append(block.get(0).substring(this.specialCharCount));
-      while ((block.get(paragIndex) != null)
-          && !block.get(paragIndex).startsWith(this.specialChar)) {
-        parag.append(block.get(paragIndex));
-        paragIndex++;
-      }
-      this.list.add(new Paragraph(parag.toString()));
-
+      int paragIndex = addParag(block);
       bqHelper(block.subList(paragIndex, block.size()), 0);
+    } else {
+      bqHelper(block, 0);
     }
+  }
+
+  private int addParag(List<String> block) {
+    int paragIndex = 1;
+    StringBuilder parag = new StringBuilder();
+    parag.append(block.get(0).substring(this.specialCharCount));
+    while ((paragIndex < block.size()) && (!block.get(paragIndex).startsWith(this.specialChar)
+        || (specialCount(block.get(paragIndex)) == this.specialCharCount)
+        || (specialCount(block.get(paragIndex)) == -1))) {
+      if (specialCount(block.get(paragIndex)) == -1) {
+        (parag.append(" ")).append(block.get(paragIndex));
+      } else {
+        parag.append(block.get(paragIndex).substring(this.specialCharCount));
+      }
+      paragIndex++;
+    }
+    this.list.add(new Paragraph(parag.toString()));
+    return paragIndex;
   }
 
 
   private void bqHelper(List<String> block, int index) {
-    if (!block.isEmpty()) {
-      if (specialCount(block.get(index)) > this.specialCharCount) {
-        bqHelper(block, index + 1);
-      } else if (index > 0) {
-        this.list.add(new Blockquote(block.subList(0, index), this.specialCharCount + 1));
+    // if (!block.isEmpty()) {
+    if ((index < block.size()) && ((specialCount(block.get(index)) > this.specialCharCount)
+        || (specialCount(block.get(index)) == -1))) {
+      bqHelper(block, index + 1);
+    } else if (index > 0) {
+      this.list.add(new Blockquote(block.subList(0, index), this.specialCharCount + 1));
+      if (index < block.size()) {
+        int paragIndex = addParag(block.subList(index, block.size()));
+        bqHelper(block.subList(paragIndex + index, block.size()), 0);
       }
-
+    } else if (!block.isEmpty()) {
+      int paragIndex = addParag(block.subList(index, block.size()));
+      bqHelper(block.subList(paragIndex, block.size()), 0);
     }
+    // }
   }
 
 
@@ -50,9 +69,38 @@ public class Blockquote extends AbstractParagraph {
     while ((specialCount < s.length()) && (s.charAt(specialCount) == '>')) {
       specialCount++;
     }
+    if (s.charAt(specialCount) != ' ') {
+      return -1;
+    }
     return specialCount;
   }
 
 
+  /**
+   * {@inheritDoc }
+   */
+  @Override
+  public String toPrettyString() {
+    StringBuilder stringBuilder = new StringBuilder();
+    stringBuilder.append("<blockquote>");
+    for (AbstractParagraph p : this.list) {
+      stringBuilder.append(AbstractElement.emphasize(p.toPrettyString()));
+    }
+    stringBuilder.append("</blockquote>\n");
+    return stringBuilder.toString();
+  }
 
+  public static void main(String[] args) {
+    List<String> test = new ArrayList<>();
+    test.add("> What are the resources allocated?");
+    test.add(">> Are you asking in terms of headcount or machines?");
+    test.add("just text");
+    test.add(">>>Headcount");
+    test.add("more text");
+    test.add(">>>>> jump from 2nd to 5th");
+    test.add("> 1st level again");
+
+    Blockquote bq = new Blockquote(test);
+    System.out.println(bq.toPrettyString());
+  }
 }
